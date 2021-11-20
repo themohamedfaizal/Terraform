6178bd44101c596ebe231e86b3ec971d12ec107a
Mohamed Faizal <91851332+themohamedfaizal@users.noreply.github.com>
2021-11-20 18:51:32 +0530

    Add files via upload

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region
-  access_key = file(../access_key.txt)
-  secret_key = file(../secret_key.txt)
+  access_key = file("../../../access_key.txt")
+  secret_key = file("../../../secret_key.txt")
 }
